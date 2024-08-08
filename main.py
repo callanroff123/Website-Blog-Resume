5cea4ab4f50458f76281bb9568d16b7b751c12ff
callanroff123 <callanroff@gmail.com>
2024-08-08 20:13:00 +1000

    rsdvrver

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import os
 from dotenv import load_dotenv
 import smtplib
 from email.message import EmailMessage
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 load_dotenv()
@@ -38,6 +40,28 @@ app.config['CKEDITOR_PKG_TYPE'] = 'full'
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+# Logging manager
+try:
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
+except:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log',
+        maxBytes=10240, 
+        backupCount=10
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Microblog startup')
 
 
 def allowed_file(filename):
